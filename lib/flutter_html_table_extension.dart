86eb540779257833eb060bc958e6bb3f6ba41d37
romancores <225000@niuitmo.ru>
2024-02-20 11:08:41 -0500

    lower sdk version added customizable styles and horizontal scroll

--- lib/flutter_html_table_extension.dart
+++ lib/flutter_html_table_extension.dart
@@ -1,9 +1,21 @@
 library flutter_html_table_extension;
 
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 
 class HtmlTableExtension extends HtmlExtension {
+  final TextStyle? headerStyle;
+  final TextStyle? cellStyle;
+  final TableBorder? tableBorder;
+
+  const HtmlTableExtension({
+    this.headerStyle,
+    this.cellStyle,
+    this.tableBorder,
+  });
+
   @override
   Set<String> get supportedTags => {"table"};
 
@@ -21,9 +33,7 @@ class HtmlTableExtension extends HtmlExtension {
         .toList();
 
     for (var list in list) {
-      list.removeWhere((element) =>
-          element.isEmpty ||
-          (element is String && (element as String).trim().isEmpty));
+      list.removeWhere((element) => element.isEmpty || (element is String && (element as String).trim().isEmpty));
     }
 
     list.removeWhere((element) => element.isEmpty);
@@ -36,18 +46,29 @@ class HtmlTableExtension extends HtmlExtension {
     return WidgetSpan(
         child: Padding(
             padding: const EdgeInsets.symmetric(vertical: 24),
-            child: DataTable(
-              key: const Key("DataTable"),
-              border: TableBorder.all(width: 1),
-              columns: List.generate(
-                  columns.length, (i) => DataColumn(label: Text(columns[i]))),
-              rows: List.generate(
-                rows.length,
-                (i) => DataRow(
-                    cells: List.generate(
-                        rows[i].length, (j) => DataCell(Text(rows[i][j])))),
-              ),
-            )));
+            child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: DataTable(
+                  key: const Key("DataTable"),
+                  border: tableBorder ?? TableBorder.all(width: 1),
+                  columns: List.generate(
+                      columns.length,
+                      (i) => DataColumn(
+                              label: Text(
+                            columns[i].trim(),
+                            style: headerStyle,
+                          ))),
+                  rows: List.generate(
+                    rows.length,
+                    (i) => DataRow(
+                        cells: List.generate(
+                            rows[i].length,
+                            (j) => DataCell(Text(
+                                  rows[i][j].trim(),
+                                  style: cellStyle,
+                                )))),
+                  ),
+                ))));
   }
 
   @override
